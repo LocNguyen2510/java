b3cd42fd20eb2135852ae666ff4df2773624823d
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-07-12 17:36:46 +0700

    day5

---
+++ src/day5/practice14.java
@@ -0,0 +1,14 @@
+package day5;
+
+import java.util.Arrays;
+
+public class practice14 {
+	public static void main(String[] args) {
+		String[] club = { "Banh", "keo", "com" };
+		System.out.println("chuoi string la: " + Arrays.toString(club));
+		club[0] = "An vat";
+		System.out.println("chuoi string 1 la: " + club[0]);
+		System.out.println("chuoi string la: " + Arrays.toString(club));
+
+	}
+}
