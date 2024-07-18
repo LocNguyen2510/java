78d829774412ae251cbb45e5734aa570e511dc4e
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-07-18 17:06:16 +0700

    practiceday7

---
+++ src/chapter4/Student.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+public class Student {
+	String name;
+	int age;
+	String fullName;
+	String firstName;
+	String emailAddress;
+
+	public void learnJava() {
+		System.out.println("Learn Java with me");
+	}
+
+	String getAge() {
+		return "DinhLoc";
+	}
+}
