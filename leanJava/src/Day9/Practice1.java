646a18f967bfc5990464881b0470efc5a5a0688c
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-08-17 16:25:10 +0700

    ArrayList

---
+++ leanJava/src/Day9/Practice1.java
@@ -0,0 +1,16 @@
+package Day9;
+
+import java.util.ArrayList;
+
+public class Practice1 {
+	public static void main(String[] args) {
+
+		ArrayList<String> a1 = new ArrayList<String>();
+		a1.add("Nguyen Dinh Loc");
+		a1.add("arrayList");
+		a1.add("Sourt");
+		a1.remove(1);
+		System.out.println(a1 + " " + a1.size());
+
+	}
+}
