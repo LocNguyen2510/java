af3aa4cce9dbcb561d2cb8661cdb63586dfd64fc
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-08-28 15:52:47 +0700

    learn Polymorphism

--- leanJava/src/Day14/Practice1.java
+++ leanJava/src/Day14/Practice1.java
@@ -1,5 +1,27 @@
 package Day14;
 
+class Animal {
+	void eat() {
+		System.out.println("animal eating");
+	}
+}
+
+class Dog extends Animal {
+	void eat() {
+		System.out.println("Dog eating");
+	}
+}
+
+class babyDog extends Dog {
+	void eat() {
+		System.out.println("BabyDog eating");
+	}
+}
+
 public class Practice1 {
+	public static void main(String[] args) {
+		Animal a = new babyDog();
+		a.eat();
+	}
 
 }
