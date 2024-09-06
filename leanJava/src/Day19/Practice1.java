345eb57d216f211743e4e347b438a9af54bbf336
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-09-06 16:33:57 +0700

    learn Try Catch

---
+++ leanJava/src/Day19/Practice1.java
@@ -0,0 +1,27 @@
+package Day19;
+
+import java.util.Scanner;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("Nhap X= ");
+			try {
+				int x = scanner.nextInt();
+				try {
+					System.out.println("ket qua la:" + 10 / x);
+					break;
+				} catch (Exception e) {
+					System.out.println("khong nhap x = 0");
+					continue;
+				}
+			} catch (Exception e) {
+				scanner.next();
+				System.out.println("Khong nhap chữ");
+
+			}
+
+		}
+	}
+}
