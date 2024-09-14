5475c9458b25c3a8649400f7cd51ee52a04ff2e2
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-09-14 10:24:05 +0700

    Learn more practice

---
+++ leanJava/src/LearnmorePractice/Sum.java
@@ -0,0 +1,16 @@
+package LearnmorePractice;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n;
+		n = scanner.nextInt();
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum = sum + i;
+		}
+		scanner.close();
+	}
+}
---
+++ leanJava/src/LearnmorePractice/TamGiacVuong.java
@@ -0,0 +1,24 @@
+package LearnmorePractice;
+
+import java.util.Scanner;
+
+public class TamGiacVuong {
+	public static void main(String[] args) {
+		int a, b, c;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Nhap a");
+		a = scan.nextInt();
+		System.out.println("Nhap b");
+		b = scan.nextInt();
+		System.out.println("Nhap c");
+		c = scan.nextInt();
+		if (a + b > c && b + c > a && c + a > b) {
+			System.out.println("Ba số đúng là 3 cạnh của 1 tam giác");
+		} else {
+			System.out.println("Ba số không phải là 3 cạnh của 1 tam giác");
+		}
+		scan.close();
+
+	}
+
+}
