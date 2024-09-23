476ac798599feeb13ae3d7aa3de8ab46e62abc40
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-09-23 12:35:20 +0700

    Fibonacci

---
+++ leanJava/src/LearnmorePractice/Fibonacci.java
@@ -0,0 +1,26 @@
+package LearnmorePractice;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+	public int CalculateFi(int n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		return CalculateFi(n - 1) + CalculateFi(n - 2);
+	}
+
+	public static void main(String[] args) {
+		int n;
+		int Fi;
+		System.out.println("Nhập vào số nguyên:");
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+
+		Fibonacci fibonacci = new Fibonacci();
+		Fi = fibonacci.CalculateFi(n);
+
+		System.out.println("Fi(" + n + ") = " + Fi);
+	}
+}
