d6d4915a613a8008db01f09bc6edf5df3f79d564
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-10-13 17:05:24 +0700

    Pratice2

---
+++ leanJava/src/LearnMorePractice2/PhanNguyenVaPhanDu.java
@@ -0,0 +1,21 @@
+package LearnMorePractice2;
+
+public class PhanNguyenVaPhanDu {
+
+	public static void main(String[] args) {
+
+		int divid = 20;
+
+		int divis = 3;
+
+		int quotient = divid / divis;
+
+		int remainder = divid % divis;
+
+		System.out.println("Phần nguyên khi chia " + divid + " cho " + divis + " là: " + quotient);
+
+		System.out.println("Phần dư khi chia " + divid + " cho " + divis + " là: " + remainder);
+
+	}
+
+}
--- leanJava/src/LearnmorePractice/String.java
+++ leanJava/src/LearnmorePractice/String.java
@@ -1,28 +1,26 @@
 package LearnmorePractice;
 
-import java.util.Scanner;
-
 public class String {
 
-	public static void main(String[] args) {
-		String chuoi;
-		char kiTu;
-		boolean tonTai = false;
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Nhập vào một chuỗi: ");
-		chuoi = scanner.nextLine();
-		System.out.println("Nhập vào ký tự muốn kiểm tra:");
-		kiTu = scanner.nextLine().charAt(0);
-
-		char mangKiTu[] = chuoi.toCharArray();
-		for (int i = 0; i < mangKiTu.length; i++) {
-			if (kiTu == mangKiTu[i]) {
-				System.out.println("Có");
-				tonTai = true;
-			}
-		}
-		if (tonTai == false)
-			System.out.println("Không");
-	}
+//	public static void main(String[] args) {
+//		String chuoi;
+//		char kiTu;
+//		boolean tonTai = false;
+//		Scanner scanner = new Scanner(System.in);
+//
+//		System.out.println("Nhập vào một chuỗi: ");
+//		chuoi = scanner.nextLine();
+//		System.out.println("Nhập vào ký tự muốn kiểm tra:");
+//		kiTu = scanner.nextLine().charAt(0);
+//
+//		char mangKiTu[] = chuoi.toCharArray();
+//		for (int i = 0; i < mangKiTu.length; i++) {
+//			if (kiTu == mangKiTu[i]) {
+//				System.out.println("Có");
+//				tonTai = true;
+//			}
+//		}
+//		if (tonTai == false)
+//			System.out.println("Không");
+//	}
 }
